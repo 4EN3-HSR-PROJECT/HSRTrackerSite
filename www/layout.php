2419c0cefa7cf0cf1cf85d8df62c616d61ef7cb3
Matt Fielding <mattfielding@cogeco.ca>
2012-09-25 20:05:44 -0400

    Formatting for multiple pages fixed
    Final commit before modification to page contents

--- www/layout.php
+++ www/layout.php
@@ -38,23 +38,39 @@ function site_menubar ($selected) {
 	$page_menubar = '<div id="menubar">
 					<table cellpadding=8><tr>
 					<td>
-						<a href="?page=main">
+						<a href="?page=main" style="text-decoration: none">
+						<div id="menubar_entry">
+						<b>
 						Home
+						</b>
+						</div>
 						</a>
 					</td>
 					<td>
-						<a href="?page=bus">
+						<a href="?page=bus" style="text-decoration: none">
+						<div id="menubar_entry">
+						<b>
 						Find a Bus
+						</b>
+						</div>
 						</a>
 					</td>
 					<td>
-						<a href="?page=taxi">
+						<a href="?page=taxi" style="text-decoration: none">
+						<div id="menubar_entry">
+						<b>
 						Find a Taxi
+						</b>
+						</div>
 						</a>
 					</td>
 					<td>
-						<a href="?page=plan">
+						<a href="?page=plan" style="text-decoration: none">
+						<div id="menubar_entry">
+						<b>
 						Plan your Trip
+						</b>
+						</div>
 						</a>
 					</td>
 					</tr></table>
