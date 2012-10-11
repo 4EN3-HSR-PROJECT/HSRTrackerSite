76b6655cdfd5660b08574503acdf652844eddf56
Matt Fielding <mattfielding@cogeco.ca>
2012-10-11 12:56:12 -0400

    Survey nearly completed
    Survey forms added
    Form submission added with error checking

---
+++ www/survey/submit.php
@@ -0,0 +1,82 @@
+<?php
+
+$error_loc = NULL;
+function error ($loc) {
+	if ($error_loc == NULL) {
+		$error_loc = $loc;
+	}
+}
+
+
+/********************
+ * Check for errors *
+ ********************/
+$email = 'NULL';
+if (isset($_POST['email'])) {
+	$email = $_POST['email'];
+}
+
+if ($_POST['freq'] == NULL) {
+	error('freq');
+} else if ($_POST['length'] == NULL) {
+	error('length');
+} else if ($_POST['phonecheck'] == NULL) {
+	error('phonecheck');
+} else if ($_POST['home'] == NULL) {
+	error('home');
+} else if ($_POST['phonecheck'] == NULL) {
+	error('phonecheck');
+} else if (!isset($_POST['tester'])) {
+	error('tester');
+} else if ($_POST['tester']) {
+	if (!isset($_POST['email']) | $_POST['email'] == "") {
+		error('email');
+	}
+}
+
+
+/***************************
+ * Submit data to database *
+ ***************************/
+// Obtain database data
+$db_name = 'hsr_stats';
+$db_table = 'survey';
+$query = "INSERT INTO {$db_table} (
+	use_school,
+	use_work,
+	use_other,
+	freq,
+	route_51,
+	route_1A,
+	route_5C,
+	route_bline,
+	length,
+	phonecheck,
+	home,
+	email
+) VALUES (
+	{$_POST['use_school']},
+	{$_POST['use_work']},
+	{$_POST['use_other']},
+	{$_POST['freq']},
+	{$_POST['route_51']},
+	{$_POST['route_1A']},
+	{$_POST['route_5C']},
+	{$_POST['route_bline']},
+	{$_POST['length']},
+	{$_POST['phonecheck']},
+	{$_POST['home']},
+	{$email}
+)";
+
+// Connect to database
+include '/var/www/db.php';
+$connected = mysql_query('use ' . $db_name);
+if (!$connected) {
+	die('Could not connect to database: ' . mysql_error());
+}
+
+// Insert data into database
+$result = mysql_query();
+
+?>
--- www/survey/survey.php
+++ www/survey/survey.php
@@ -1,29 +1,92 @@
+<script>
+function emailBox (status) {
+	var output = '';
+	if (status == 1) {
+		output += 'E-Mail Address:';
+		output += '<div id="surveyanswer">';
+		output += '<input type="text" name="email" maxlength=256 size=40';
+		output += '></div>';
+	}
+	document.getElementById('emailsect').innerHTML = output;
+}
+</script>
+
 <form action="survey/submit.php" method="post">
-<table>
-<tr>
-	<td>First name:</td>
-	<td><input type="text" name="firstname"></td>
-</tr>
-<tr>
-	<td>Last name:</td>
-	<td><input type="text" name="lastname"></td>
-</tr>
-<tr>
-	<td>Gender:</td>
-	<td>
-		<input type="radio" name="sex" value="male">Male<br>
-		<input type="radio" name="sex" value="female">Female
-	</td>
-</tr>
-<tr>
-	<td>I have a...</td>
-	<td>
-		<input type="checkbox" name="vehicle" value="Bike">Bicycle<br>
-		<input type="checkbox" name="vehicle" value="Car">Car
-	</td>
-</tr>
-<tr>
-	<td colspan=2 align="center"><input type="submit" value="Submit"></td>
-</tr>
-</table>
+
+What do you use the bus for?
+<div id="surveyanswer">
+	<input type="checkbox" name="use_school"> School<br>
+	<input type="checkbox" name="use_work"> Work<br>
+	<input type="checkbox" name="use_other"> Other<br>
+</div><br><br>
+
+How often do you take the bus per day?
+<div id="surveyanswer">
+<select name="freq">
+	<option value=NULL style="display:none;"></option>
+	<option value=0>0</option>
+	<option value=1>1-3</option>
+	<option value=4>4-6</option>
+	<option value=7>7+</option>
+	<option value=-1>A few times a week</option>
+</select>
+</div><br><br>
+
+Which of the following buses do you take most often?
+<div id="surveyanswer">
+	<input type="checkbox" name="route_51"> 51 - University<br>
+	<input type="checkbox" name="route_1A"> 1A - King<br>
+	<input type="checkbox" name="route_5C"> 5C - Delaware<br>
+	<input type="checkbox" name="route_bline"> B-Line<br>
+</div><br><br>
+
+On average, how long is your bus ride?
+<div id="surveyanswer">
+<select name="length">
+	<option value=NULL style="display:none;"></option>
+	<option value=5>5 minutes</option>
+	<option value=10>10 minutes</option>
+	<option value=15>15 minutes</option>
+	<option value=30>30 minutes</option>
+	<option value=45>45+ minutes</option>
+</select>
+</div><br><br>
+
+How often do you check your phone during your bus ride?
+<div id="surveyanswer">
+<select name="phonecheck">
+	<option value=NULL style="display:none;"></option>
+	<option value=1>Constantly</option>
+	<option value=5>Every 5 minutes</option>
+	<option value=10>Every 10+ minutes</option>
+	<option value=0>Never</option>
+</select>
+</div><br><br>
+
+In what area do you live?
+<div id="surveyanswer">
+<select name="home">
+	<option value=NULL style="display:none;"></option>
+	<option value=1>McMaster Area</option>
+	<option value=2>Downtown Hamilton</option>
+	<option value=3>Dundas</option>
+	<option value=4>Ancaster</option>
+	<option value=5>Stoney Creek</option>
+	<option value=6>Flamborough</option>
+	<option value=0>Other</option>
+</select>
+</div><br><br>
+
+Would you consider helping us test this app?
+<div id="surveyanswer">
+	<input type="radio" name="tester" value=1 onclick="emailBox(1)" checked> Yes<br>
+	<input type="radio" name="tester" value=0 onclick="emailBox(0)"> No
+</div><br>
+<div id="emailsect">
+E-Mail Address:
+<div id="surveyanswer">
+	<input type="text" name="email" maxlength=256 size=40>
+</div>
+</div>
+
 </form>
