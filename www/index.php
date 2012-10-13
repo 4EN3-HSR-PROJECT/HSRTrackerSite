30a6376c166956e11fa7f74e973ff8b243dc5aec
Sasitha kaludeva <sasi@hotmail.ca>
2012-10-13 15:37:26 -0400

    trying to remove main menu

--- www/index.php
+++ www/index.php
@@ -6,8 +6,8 @@
  ************************/
 include 'interface_check.php';
 $included_interface = function_exists('getInterface');
-// include 'layout.php';
-$included_layout = function_exists('site_header');
+//$include 'layout.php';
+//$included_layout = function_exists('site_header');
 
 /***********************
  * Determine Interface *
