72e6138cb5735211a57ac23e75a73c023fe761b0
Matt Fielding <mattfielding@cogeco.ca>
2012-10-18 12:42:50 -0400

    Survey completed
    Added: Error checking with visible flags
    Added: Survey completion on failed submission

--- www/survey/submit.php
+++ www/survey/submit.php
@@ -1,5 +1,4 @@
 <?php
-
 /**************************
  * Initialize error flags *
  **************************/
@@ -11,99 +10,86 @@ $error['tester'] = false;
 $error['email'] = false;
 
 
-/*****************************
- * Get variables from $_POST *
- *****************************/
-/*$freq = NULL;
-if (isset($_POST['freq'])) {$freq = $_POST['freq']}
-$length = NULL;
-if (isset($_POST['length'])) {$length = $_POST['length']}
-$home = NULL;
-if (isset($_POST['home'])) {$home = $_POST['home']}
-$phonecheck = NULL;
-if (isset($_POST['phonecheck'])) {$phonecheck = $_POST['phonecheck']}
-$tester = NULL;
-if (isset($_POST['tester'])) {$tester = $_POST['tester']}
-$email = NULL;
-if (isset($_POST['email'])) {$email = $_POST['email']}*/
-
-
 /********************
  * Check for errors *
  ********************/
-if ($_POST['freq'] == NULL) {
-	$error['freq'] = true;
-} else if ($_POST['length'] == NULL) {
-	$error['length'] = true;
-} else if ($_POST['home'] == NULL) {
-	$error['home'] = true;
-} else if ($_POST['phonecheck'] == NULL) {
-	$error['phonecheck'] = true;
-} else if (!isset($_POST['tester'])) {
-	$error['tester'] = true;
-} else if ($_POST['tester']) {
-	if (!isset($_POST['email']) | $_POST['email'] == "") {
-		$error['email'] = true;
-	}
+$email = "";
+if (isset($_POST['email'])) {
+	$email = $_POST['email'];
 }
 
-// If an error has occurred, redirect back to the form
-if (in_array(true,$error)) {
-	echo 'Error<br>';
+if ($_POST['freq'] == NULL | $_POST['freq'] == "NULL") {
+	$error['freq'] = true;
+}
+if ($_POST['length'] == "NULL") {
+	$error['length'] = true;
+}
+if ($_POST['phonecheck'] == "NULL") {
+	$error['phonecheck'] = true;
+}
+if ($_POST['home'] == "NULL") {
+	$error['home'] = true;
+}
+if (!isset($_POST['tester'])) {
+	$error['tester'] = true;
 } else {
-	echo 'No error<br>';
+	if ($email == "") {
+		$error['email'] = true;
+	}
 }
 
 
 /***************************
  * Submit data to database *
  ***************************/
-// Obtain database data
-$db_name = 'hsr_stats';
-$db_table = 'survey';
-$query = "INSERT INTO {$db_table} (
-	use_school,
-	use_work,
-	use_other,
-	freq,
-	route_51,
-	route_1A,
-	route_5C,
-	route_bline,
-	length,
-	phonecheck,
-	home,
-	email
-) VALUES (
-	{$_POST['use_school']},
-	{$_POST['use_work']},
-	{$_POST['use_other']},
-	{$_POST['freq']},
-	{$_POST['route_51']},
-	{$_POST['route_1A']},
-	{$_POST['route_5C']},
-	{$_POST['route_bline']},
-	{$_POST['length']},
-	{$_POST['phonecheck']},
-	{$_POST['home']},
-	\"{$email}\"
-)";
+function submit () {
+	// Obtain database data
+	$db_name = 'hsr_stats';
+	$db_table = 'survey';
+	$query = "INSERT INTO {$db_table} (
+		use_school,
+		use_work,
+		use_other,
+		freq,
+		route_51,
+		route_1A,
+		route_5C,
+		route_bline,
+		length,
+		phonecheck,
+		home,
+		email
+	) VALUES (
+		{$_POST['use_school']},
+		{$_POST['use_work']},
+		{$_POST['use_other']},
+		{$_POST['freq']},
+		{$_POST['route_51']},
+		{$_POST['route_1A']},
+		{$_POST['route_5C']},
+		{$_POST['route_bline']},
+		{$_POST['length']},
+		{$_POST['phonecheck']},
+		{$_POST['home']},
+		\"{$email}\"
+	)";
 
-// Connect to database
-include '/var/www/db.php';
-$connected = mysql_query('use ' . $db_name);
-if (!$connected) {
-	die('Could not connect to database: ' . mysql_error());
+	// Connect to database
+	include '/var/www/db.php';
+	$connected = mysql_query('use ' . $db_name);
+	if (!$connected) {
+		die('Could not connect to database: ' . mysql_error());
+	}
+
+	// Insert data into database
+	$result = mysql_query($query);
+	if (!$result) {
+		return false;
+		die('Invalid query: ' . mysql_error() . '<br>'
+			. 'Full Query: ' . $query);
+	}
+
+	return true;
 }
-
-// Insert data into database
-$result = mysql_query($query);
-if (!$result) {
-	die('Invalid query: ' . mysql_error() . '<br>'
-		. 'Full Query: ' . $query);
-}
-
-// Show completion message
-echo 'Your survey has been submitted successfully!';
 
 ?>
--- www/survey/survey.php
+++ www/survey/survey.php
@@ -1,3 +1,59 @@
+<?php
+	// Determine if any errors exist from submission
+	function error ($str) {
+		global $error;
+		if (isset($error)) {
+			return ($error[$str]);
+		} else {
+			return false;
+		}
+	}
+	
+	// Select previously submitted values
+	function select_check ($entry) {
+		if(isset($_POST[$entry])) {
+			if ($_POST[$entry]) {
+				return 'checked';
+			}
+		}
+		return '';
+	}
+	
+	function select_dropdown ($menu, $entry) {
+		if (isset($_POST[$menu])) {
+			if ($_POST[$menu] == $entry) {
+				return 'selected';
+			}
+		}
+		return '';
+	}
+	
+	function select_radio ($set, $entry) {
+		if (isset($_POST[$set])) {
+			if ($_POST[$set] == $entry) {
+				return 'checked';
+			}
+		}
+		return '';
+	}
+	
+	// If submission, check values
+	if (isset($_POST['submitted'])) {
+		include 'survey/submit.php';
+		if (!in_array(true,$error)) {
+			// Submit query
+			if (submit()) {
+				// Successful submission
+				echo 'Survey submitted successfully!';
+			} else {
+				// Failed submission
+				echo 'An error has occurred! Please notify the system administrator.';
+			}
+			die();
+		}
+	}
+?>
+
 <script>
 function emailBox (status) {
 	var output = '';
@@ -11,88 +67,101 @@ function emailBox (status) {
 }
 </script>
 
-<form action="survey/submit.php" method="post">
+<form action="" method="post">
+
+<!-- Hidden submission variable -->
+<input type="hidden" name="submitted">
 
 What do you use the bus for?
 <div id="surveyanswer">
 	<input type="hidden" name="use_school" value=0>
-	<input type="checkbox" name="use_school" value=1> School<br>
+	<input type="checkbox" name="use_school" value=1 <?php echo select_check('use_school') ?>> School<br>
 	<input type="hidden" name="use_work" value=0>
-	<input type="checkbox" name="use_work" value=1> Work<br>
+	<input type="checkbox" name="use_work" value=1 <?php echo select_check('use_work') ?>> Work<br>
 	<input type="hidden" name="use_other" value=0>
-	<input type="checkbox" name="use_other" value=1> Other<br>
+	<input type="checkbox" name="use_other" value=1 <?php echo select_check('use_other') ?>> Other<br>
 </div><br><br>
 
+<?php if (error('freq')) {echo '<img src="error.png">';} ?>
 How often do you take the bus per day?
 <div id="surveyanswer">
 <select name="freq">
 	<option value=NULL style="display:none;"></option>
-	<option value=0>0</option>
-	<option value=1>1-3</option>
-	<option value=4>4-6</option>
-	<option value=7>7+</option>
-	<option value=-1>A few times a week</option>
+	<option value=0 <?php echo select_dropdown('freq',0) ?>>0</option>
+	<option value=1 <?php echo select_dropdown('freq',1) ?>>1-3</option>
+	<option value=4 <?php echo select_dropdown('freq',4) ?>>4-6</option>
+	<option value=7 <?php echo select_dropdown('freq',7) ?>>7+</option>
+	<option value=-1 <?php echo select_dropdown('freq',-1) ?>>A few times a week</option>
 </select>
 </div><br><br>
 
 Which of the following buses do you take most often?
 <div id="surveyanswer">
 	<input type="hidden" name="route_51" value="0">
-	<input type="checkbox" name="route_51" value=1> 51 - University<br>
+	<input type="checkbox" name="route_51" value=1 <?php echo select_check('route_51') ?>> 51 - University<br>
 	<input type="hidden" name="route_1A" value="0">
-	<input type="checkbox" name="route_1A" value=1> 1A - King<br>
+	<input type="checkbox" name="route_1A" value=1 <?php echo select_check('route_1A') ?>> 1A - King<br>
 	<input type="hidden" name="route_5C" value="0">
-	<input type="checkbox" name="route_5C" value=1> 5C - Delaware<br>
+	<input type="checkbox" name="route_5C" value=1 <?php echo select_check('route_5C') ?>> 5C - Delaware<br>
 	<input type="hidden" name="route_bline" value="0">
-	<input type="checkbox" name="route_bline" value=1> B-Line<br>
+	<input type="checkbox" name="route_bline" value=1 <?php echo select_check('route_bline') ?>> B-Line<br>
 </div><br><br>
 
+<?php if (error('length')) {echo '<img src="error.png">';} ?>
 On average, how long is your bus ride?
 <div id="surveyanswer">
 <select name="length">
 	<option value=NULL style="display:none;"></option>
-	<option value=5>5 minutes</option>
-	<option value=10>10 minutes</option>
-	<option value=15>15 minutes</option>
-	<option value=30>30 minutes</option>
-	<option value=45>45+ minutes</option>
+	<option value=5 <?php echo select_dropdown('length',5) ?>>5 minutes</option>
+	<option value=10 <?php echo select_dropdown('length',10) ?>>10 minutes</option>
+	<option value=15 <?php echo select_dropdown('length',15) ?>>15 minutes</option>
+	<option value=30 <?php echo select_dropdown('length',30) ?>>30 minutes</option>
+	<option value=45 <?php echo select_dropdown('length',45) ?>>45+ minutes</option>
 </select>
 </div><br><br>
 
+<?php if (error('phonecheck')) {echo '<img src="error.png">';} ?>
 How often do you check your phone during your bus ride?
 <div id="surveyanswer">
 <select name="phonecheck">
 	<option value=NULL style="display:none;"></option>
-	<option value=1>Constantly</option>
-	<option value=5>Every 5 minutes</option>
-	<option value=10>Every 10+ minutes</option>
-	<option value=0>Never</option>
+	<option value=1 <?php echo select_dropdown('phonecheck',1) ?>>Constantly</option>
+	<option value=5 <?php echo select_dropdown('phonecheck',5) ?>>Every 5 minutes</option>
+	<option value=10 <?php echo select_dropdown('phonecheck',10) ?>>Every 10+ minutes</option>
+	<option value=0 <?php echo select_dropdown('phonecheck',0) ?>>Never</option>
 </select>
 </div><br><br>
 
+<?php if (error('home')) {echo '<img src="error.png">';} ?>
 In what area do you live?
 <div id="surveyanswer">
 <select name="home">
 	<option value=NULL style="display:none;"></option>
-	<option value=1>McMaster Area</option>
-	<option value=2>Downtown Hamilton</option>
-	<option value=3>Dundas</option>
-	<option value=4>Ancaster</option>
-	<option value=5>Stoney Creek</option>
-	<option value=6>Flamborough</option>
-	<option value=0>Other</option>
+	<option value=1 <?php echo select_dropdown('home',1) ?>>McMaster Area</option>
+	<option value=2 <?php echo select_dropdown('home',2) ?>>Downtown Hamilton</option>
+	<option value=3 <?php echo select_dropdown('home',3) ?>>Dundas</option>
+	<option value=4 <?php echo select_dropdown('home',4) ?>>Ancaster</option>
+	<option value=5 <?php echo select_dropdown('home',5) ?>>Stoney Creek</option>
+	<option value=6 <?php echo select_dropdown('home',6) ?>>Flamborough</option>
+	<option value=0 <?php echo select_dropdown('home',0) ?>>Other</option>
 </select>
 </div><br><br>
 
+<?php if (error('tester')) {echo '<img src="error.png">';} ?>
 Would you consider helping us test this app?
 <div id="surveyanswer">
 	<input type="radio" name="tester" value=1 onclick="emailBox(1)" checked> Yes<br>
 	<input type="radio" name="tester" value=0 onclick="emailBox(0)"> No
 </div><br>
 <div id="emailsect">
+<?php if (error('email')) {echo '<img src="error.png">';} ?>
 E-Mail Address:
 <div id="surveyanswer">
-	<input type="text" name="email" maxlength=256 size=40>
+	<input type="text" name="email" maxlength=256 size=40
+	<?php if (isset($_POST['email'])) {
+			echo ' value=' . $_POST['email'];
+		} ?>
+	>
 </div>
 </div><br><br>
 
